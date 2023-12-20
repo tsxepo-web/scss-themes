e5f225890e96a77195a1b8a60860fcd65596fab9
tsxepo <tsxepo@gmail.com>
2023-12-20 07:04:59 +0200

    mobile done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'scss-themes';
+  isMenuVisible = false;
+  openDropdownIndex: number | null = null;
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+  toggleDropdown(index: number) {
+    this.openDropdownIndex = this.openDropdownIndex === index ? null : index;
+  }
+  isDropdownOpen(index: number): boolean {
+    return this.openDropdownIndex === index;
+  }
 }
